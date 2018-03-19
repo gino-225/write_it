90c4324cf440ef033911e7ad76b28a0ff8fe4c54
gino-225 <george_bm@aol.com>
2018-03-19 22:15:38 +0000

    saved everthing before pulling new request

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,10 +12,7 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment = Comment.new(comment_params)
-    @comment.user = current_user
-
-
+    @comment = Comment.new
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
@@ -27,19 +24,4 @@ class CommentsController < ApplicationController
     end
   end
 
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = comment.find(params[:id])
-    end
-
-    def set_post
-      @post = Post.find(params[:post_id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def comment_params
-      params.require(:comment).permit(:content, :user_id, :post_id)
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  #resources :posts, :comments
+  # resources :posts, :comments
   devise_for :users
   root to: 'posts#index'
 
