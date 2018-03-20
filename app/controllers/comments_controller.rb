6101d3f554917c39faa46089068c94a00b8ff189
gino-225 <george_bm@aol.com>
2018-03-20 10:54:33 +0000

    nothing new

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,18 @@
 class CommentsController < ApplicationController
+ before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+#before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+
+
 
   def index
     @comments = Comment.all
   end
 
   def show
+    @comments = @post.comments.all
+    @comment = @post.comments.new
   end
 
   def new
@@ -12,7 +20,9 @@ class CommentsController < ApplicationController
   end
 
   def create
+    #@comment = Comment.new
     @comment = Comment.new
+    @comment.user = current_user
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
@@ -25,3 +35,12 @@ class CommentsController < ApplicationController
   end
 
 end
+
+private
+def set_post
+     @post = Post.find(params[:post_id])
+   end    # Use callbacks to share common setup or constraints between actions.
+
+   def comment_params
+     params.require(:post).permit(:content, :post_id, :user_id)
+   end
